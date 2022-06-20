78dce61c8ab8948e28202fd550298c1f9880fbbe
ivankaa123 <106930624+ivankaa123@users.noreply.github.com>
2022-06-20 10:14:24 -0600

    Update Jobbing-Main.py

--- Jobbing-Main.py
+++ Jobbing-Main.py
@@ -60,8 +60,6 @@ old_jobbers = 0
 old_enemy_jobbers = 0
 our_bots = []
 enemy_bots = []
-
-
 def strip_standing_data(puzzle):
     puzzle = puzzle.split('/')
     puzzle = puzzle[1].lower()
@@ -74,107 +72,94 @@ def strip_exp_data(exp):
     return exp
 
 
-# 'type' is for whether parsing a crew or a pirate.
-# 'userequest' defaults to None unless we set it to a value when calling the function
-# in which case we use the requests library instead of urllib
-def parse(type, data, userequest=None):
-    if type == 'crew':
-        URL = 'http://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=' + str(data)
-    if type == 'pirate':
-        URL = 'http://emerald.puzzlepirates.com/yoweb/pirate.wm?classic=false&target=' + str(data)
-    if userequest is not None:
-        res = requests.get("https://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=" + str(data))
-        soup = bs(res.text, "html.parser")
-        return soup
-    req = urllib.request.Request(URL, headers=hdr)
-    r = urllib.request.urlopen(req).read()
-    soup = bs(r, 'html.parser')
-    return soup
 
 
-def find_change(old, new):
-    change = int(new) - int(old)
-    if change != 0:
-        if change > 0:
-            return "+" + str(change)  # Purely for visual formatting
-        return change
-    else:
-        return 0
+
+def calculate_change(current, enemy_current):
+    with open("data.csv", 'r') as f:
+        for line in f:
+            last_line = line
+        try:
+            lv = last_line.split(',')[1]
+            enemy_lv = last_line.split(',')[3]
+            change = int(current) - int(lv)
+            e_change = int(enemy_current) - int(enemy_lv)
+            if change > 0:
+                change = '+' + str(change)
+            if e_change > 0:
+                e_change = '+' + str(e_change)
+            return change, e_change
+        except IndexError:
+            return 0, 0
 
 
-def scrape(crewname):
-    global old_jobbers
-    global old_enemy_jobbers
+def scrape():
+    global res
+    global enemy_res
+    global jobbers
+    global enemy_jobbers
     global crew
     global enemy_crew
-    soup = parse('crew', crewname, True)
-    crew_name = soup.find("b").get_text()
+    global old_jobbers
+    global old_enemy_jobbers
+    global change
+    global enemy_change
+    res = requests.get("https://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=" + str(id))
+    soup = bs(res.text, "html.parser")
+    crew = soup.find("b").get_text()
+    jobbers = soup.find(string="Jobbing Pirate:").find_next(string="Jobbing Pirate:").find_next().get_text()
+    enemy_res = requests.get("https://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=" + str(enemy_id))
+    enemy_soup = bs(enemy_res.text, "html.parser")
+    enemy_crew = enemy_soup.find("b").get_text()
+    enemy_jobbers = enemy_soup.find(string="Jobbing Pirate:").find_next(string="Jobbing Pirate:").find_next().get_text()
+    change = int(jobbers) - old_jobbers
+    enemy_change = int(enemy_jobbers) - old_enemy_jobbers
+    old_jobbers = int(jobbers)
+    old_enemy_jobbers = int(enemy_jobbers)
+
+def get_pirates(id, enemy_id):
+    global names_list
+    global enemy_names_list
+    URL = 'http://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=' + str(id)
+    enemy_URL = 'http://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=' + str(enemy_id)
+    req = urllib.request.Request(URL, headers=hdr)
+    enemy_req = urllib.request.Request(enemy_URL, headers=hdr)
+    r = urllib.request.urlopen(req).read()
+    soup = bs(r, 'html.parser')
+    pirates_list = soup.find(src='/yoweb/images/crew-jobbing.png').find_parent('tr').find_parent(
+        'tr').find_all_next('a')
+    del (pirates_list[-1])
+    names_list = []
+    for i in pirates_list:
+        names_list.append(i.string)
+    enemy_r = urllib.request.urlopen(enemy_req).read()
+    enemy_soup = bs(enemy_r, 'html.parser')
+    enemy_pirates_list = enemy_soup.find(src='/yoweb/images/crew-jobbing.png').find_parent('tr').find_parent(
+        'tr').find_all_next('a')
+    del (enemy_pirates_list[-1])
+    enemy_names_list = []
+    for i in enemy_pirates_list:
+        enemy_names_list.append(i.string)
+def return_flag_us(pirate):
+    pirate_URL = 'http://emerald.puzzlepirates.com/yoweb/pirate.wm?classic=false&target=' + pirate
+    pirate_req = urllib.request.Request(pirate_URL, headers=hdr)
+    pirate_r = urllib.request.urlopen(pirate_req).read()
+    pirate_soup = bs(pirate_r, 'html.parser')
     try:
-        jobber_num = soup.find(string="Jobbing Pirate:").find_next(string="Jobbing Pirate:").find_next().get_text()
-        jobber_num = jobber_num.strip()
-    except AttributeError:  # Handles the exception if there are no jobbers in the crew.
-        if crewname == id:
-            crew = crew_name
-            return crew_name, 0, 0
-        else:
-            enemy_crew = crewname
-            return crew_name, 0, 0
-    if crewname == id:  # Check which crew is currently being parsed and set the associated variables
-        crew = crew_name
-        change = find_change(old_jobbers, jobber_num)
-        old_jobbers = jobber_num
-        return crew_name, jobber_num, change
-
-    else:
-        enemy_crew = crew_name
-        change = find_change(old_enemy_jobbers, jobber_num)
-        old_enemy_jobbers = jobber_num
-        return crew_name, jobber_num, change
-
-
-# def scrape(): # Needs to return Jobbers, Crew, Change
-#     # global res
-#     # global enemy_res
-#     global jobbers
-#     global enemy_jobbers
-#     global crew
-#     global enemy_crew
-#     global old_jobbers
-#     global old_enemy_jobbers
-#     global change
-#     global enemy_change
-#     res = requests.get("https://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=" + str(id))
-#     soup = bs(res.text, "html.parser")
-#     crew = soup.find("b").get_text()
-#     jobbers = soup.find(string="Jobbing Pirate:").find_next(string="Jobbing Pirate:").find_next().get_text()
-#     enemy_res = requests.get("https://emerald.puzzlepirates.com/yoweb/crew/info.wm?crewid=" + str(enemy_id))
-#     enemy_soup = bs(enemy_res.text, "html.parser")
-#     enemy_crew = enemy_soup.find("b").get_text()
-#     enemy_jobbers = enemy_soup.find(string="Jobbing Pirate:").find_next(string="Jobbing Pirate:").find_next().get_text()
-#     change = int(jobbers) - old_jobbers
-#     enemy_change = int(enemy_jobbers) - old_enemy_jobbers
-#     old_jobbers = int(jobbers)
-#     old_enemy_jobbers = int(enemy_jobbers)
-
-def get_jobber_names(id):
-    soup = parse('crew', id)
-    try:
-        pirates_list = soup.find(src='/yoweb/images/crew-jobbing.png').find_parent('tr').find_parent(
-            'tr').find_all_next('a')
-        del (pirates_list[-1])
-        names_list = []
-        for i in pirates_list:
-            names_list.append(i.string)
-        return names_list
-    except AttributeError:
-        pass
-
-
-def bot_check(pirate):
-    pirate_soup = parse('pirate', pirate)
+        flag = pirate_soup.find('a', href=re.compile('^/yoweb/flag')).string
+    except:
+        flag = 'Independent'
+    global flag_counts
+    if flag in flag_counts:
+        flag_counts[flag] += 1
+    if flag not in flag_counts:
+        flag_counts[flag] = 1
+    global our_bots
     data = pirate_soup.get_text()
-    data = data.split('Piracy Skills')[1]
-    data = data.split('Carousing Skills')[0]
+    data = data.split('Piracy Skills')
+    data = data[1]
+    data = data.split('Carousing Skills')
+    data = data[0]
     data = data.split()
     no_arch = [x for x in data if "archipelago" not in x]
     no_arch = [x for x in data if "/" in x]
@@ -183,6 +168,8 @@ def bot_check(pirate):
     carpentry = str(no_arch[2])
     patching = str(no_arch[3])
     bilging = str(no_arch[4])
+    dnav = str(no_arch[7])
+
     sailing_exp = strip_exp_data(sailing)
     sailing = strip_standing_data(sailing)
     carpentry_exp = strip_exp_data(carpentry)
@@ -193,6 +180,7 @@ def bot_check(pirate):
     patching = strip_standing_data(patching)
     bilging_exp = strip_exp_data(bilging)
     bilging = strip_standing_data(bilging)
+    dnav = strip_standing_data(dnav)
     rank_score = (rank_scoring[rigging] * exp_scoring[rigging_exp]) + (
             rank_scoring[patching] * exp_scoring[patching_exp]) + (
                          rank_scoring[bilging] * exp_scoring[bilging_exp])
@@ -215,61 +203,79 @@ def bot_check(pirate):
     if (rank_scoring[rigging] >= 7) and (rank_scoring[patching] >= 7) and (rank_scoring[bilging] >= 7):
         bot_percent = bot_percent * 2
     bot_percent = round(bot_percent, 2)
-    return bot_percent
-
-
-def return_flag_us(pirate):
-    try:
-        pirate_soup = parse('pirate', pirate)
-        try:
-            flag = pirate_soup.find('a', href=re.compile('^/yoweb/flag')).string
-        except:
-            flag = 'Independent'
-        global flag_counts
-        if flag in flag_counts:
-            flag_counts[flag] += 1
-        if flag not in flag_counts:
-            flag_counts[flag] = 1
-        global our_bots
-        is_bot = bot_check(pirate)
-        if is_bot > 50:
-            our_bots.append(pirate)
-    except AttributeError:  # catches the error if we try get the flags for a crew with not jobbers
-        pass
+    if bot_percent > 50:
+        our_bots.append(pirate.string)
 
 
 def return_flag_enemy(pirate):
+    enemy_pirate_URL = 'http://emerald.puzzlepirates.com/yoweb/pirate.wm?classic=false&target=' + pirate
+    enemy_pirate_req = urllib.request.Request(enemy_pirate_URL, headers=hdr)
+    enemy_pirate_r = urllib.request.urlopen(enemy_pirate_req).read()
+    enemy_pirate_soup = bs(enemy_pirate_r, 'html.parser')
     try:
-        enemy_pirate_soup = parse('pirate', pirate)
-        try:
-            flag = enemy_pirate_soup.find('a', href=re.compile('^/yoweb/flag')).string
-        except:
-            flag = 'Independent'
-        global enemy_flag_counts
-        if flag in enemy_flag_counts:
-            enemy_flag_counts[flag] += 1
-        if flag not in enemy_flag_counts:
-            enemy_flag_counts[flag] = 1
-        global enemy_bots
-        is_bot = bot_check(pirate)
-        if is_bot > 50:
-            enemy_bots.append(pirate.string)
-    except AttributeError:  # catches exception if threading tries to get the flag of an empty list
-        pass
+        flag = enemy_pirate_soup.find('a', href=re.compile('^/yoweb/flag')).string
+    except:
+        flag = 'Independent'
+    global enemy_flag_counts
+    if flag in enemy_flag_counts:
+        enemy_flag_counts[flag] += 1
+    if flag not in enemy_flag_counts:
+        enemy_flag_counts[flag] = 1
+    global enemy_bots
+    data = enemy_pirate_soup.get_text()
+    data = data.split('Piracy Skills')
+    data = data[1]
+    data = data.split('Carousing Skills')
+    data = data[0]
+    data = data.split()
+    no_arch = [x for x in data if "archipelago" not in x]
+    no_arch = [x for x in data if "/" in x]
+    sailing = str(no_arch[0])
+    rigging = str(no_arch[1])
+    carpentry = str(no_arch[2])
+    patching = str(no_arch[3])
+    bilging = str(no_arch[4])
+    dnav = str(no_arch[7])
 
-
+    sailing_exp = strip_exp_data(sailing)
+    sailing = strip_standing_data(sailing)
+    carpentry_exp = strip_exp_data(carpentry)
+    carpentry = strip_standing_data(carpentry)
+    rigging_exp = strip_exp_data(rigging)
+    rigging = strip_standing_data(rigging)
+    patching_exp = strip_exp_data(patching)
+    patching = strip_standing_data(patching)
+    bilging_exp = strip_exp_data(bilging)
+    bilging = strip_standing_data(bilging)
+    dnav = strip_standing_data(dnav)
+    rank_score = (rank_scoring[rigging] * exp_scoring[rigging_exp]) + (
+            rank_scoring[patching] * exp_scoring[patching_exp]) + (
+                         rank_scoring[bilging] * exp_scoring[bilging_exp])
+    if exp_scoring[sailing_exp] >= 4:
+        rank_score = rank_score * 2
+    elif exp_scoring[sailing_exp] <= 2:
+        rank_score = rank_score * 0.8
+    if rank_scoring[sailing] >= 2:
+        rank_score = rank_score * 0.65
+    if exp_scoring[carpentry_exp] >= 4:
+        rank_score = rank_score * 2
+    elif exp_scoring[carpentry_exp] <= 2:
+        rank_score = rank_score * 0.9
+    if rank_scoring[carpentry] >= 4:
+        rank_score = rank_score * 0.75
+    rank_score = round(rank_score, 2)
+    bot_percent = (rank_score / 28) * 100
+    if flag == 'Independent':
+        bot_percent = bot_percent * 1.2
+    if (rank_scoring[rigging] >= 7) and (rank_scoring[patching] >= 7) and (rank_scoring[bilging] >= 7):
+        bot_percent = bot_percent * 2
+    bot_percent = round(bot_percent, 2)
+    if bot_percent > 50:
+        enemy_bots.append(pirate.string)
 @tasks.loop(minutes=1)
 async def count(ctx):
-    us = scrape(id)  # Will return a tuple of 3 values, crew_name, jobber count and change.
-    them = scrape(enemy_id)
-    if us[2] != 0:
-        await ctx.send(f"{us[0]} has {us[1]} jobbers.  Change of {us[2]}.")
-    else:
-        await ctx.send(f"{us[0]} has {us[1]} jobbers.")
-    if them[2] != 0:
-        await ctx.send(f"{them[0]} has {them[1]} jobbers.  Change of {them[2]}.")
-    else:
-        await ctx.send(f"{them[0]} has {them[1]} jobbers.")
+    scrape()
+    await ctx.send("\n" + str(crew) + " has " + str(jobbers) + " jobbers. Changed " + str(change) + "\n" + str(enemy_crew) + " has " + str(enemy_jobbers) + " jobbers. Changed " + str(enemy_change))
     await asyncio.sleep(60)
 
 
@@ -295,52 +301,31 @@ async def count_start(ctx):
     count.start(ctx)
 
 
-@bot.command(name='stop')
-async def count_stop(ctx):
-    count.cancel()
-    await ctx.send('Count stopped.')
-
-
 @bot.command(name='flags')
 async def flags(ctx):
     global flag_counts
     global enemy_flag_counts
-    names_list = get_jobber_names(id)
-    enemy_names_list = get_jobber_names(enemy_id)
-    try:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
-            executor.map(return_flag_us, names_list)
-    except TypeError:
-        pass
-    try:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=24) as enemy_executor:
-            enemy_executor.map(return_flag_enemy, enemy_names_list)
-    except TypeError:
-        pass
-    await ctx.send("**Our Jobbers: " + str(sum(flag_counts.values())) + "**")
+    get_pirates(id, enemy_id)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
+        executor.map(return_flag_us, names_list)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=24) as enemy_executor:
+        enemy_executor.map(return_flag_enemy, enemy_names_list)
+    await ctx.send("**Our Jobbers:" + str(sum(flag_counts.values())) + "**")
     for key, value in sorted(flag_counts.items(), key=itemgetter(1), reverse=True):
-        await ctx.send(str(key) + ": " + str(value))
-    await ctx.send("**Enemy Jobbers: " + str(sum(enemy_flag_counts.values())) + "**")
+        await ctx.send(str(key) + ":" + str(value))
+    await ctx.send("**Enemy Jobbers:" + str(sum(enemy_flag_counts.values())) + "**")
     for key, value in sorted(enemy_flag_counts.items(), key=itemgetter(1), reverse=True):
-        await ctx.send(str(key) + ": " + str(value))
+        await ctx.send(str(key) + ":" + str(value))
 
 
 @bot.command(name='bots')
 async def bots(ctx):
     global our_bots
     global enemy_bots
-    if len(our_bots) != 0:  # Formatting for if there are no likely bots in either crew.
-        our_bots = ', '.join(our_bots)
-        await ctx.send(f"{crew} likely bots:")
-        await ctx.send(our_bots)
-    else:
-        await ctx.send('No bots in friendly crew.')
-    if len(enemy_bots) != 0:
-        enemy_bots = ', '.join(enemy_bots)
-        await ctx.send(f"{enemy_crew} likely bots:")
-        await ctx.send(enemy_bots)
-    else:
-        await ctx.send('No bots in enemy crew.')
+    await ctx.send(str(crew) + " likely bots:")
+    await ctx.send(our_bots)
+    await ctx.send(str(enemy_crew) + " likely bots:")
+    await ctx.send(enemy_bots)
 
 
 bot.run(token)
